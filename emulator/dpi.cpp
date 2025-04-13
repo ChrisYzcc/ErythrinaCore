b0b141959d2e1033ab55bee3dca8af535b6fa63c
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-04-13 22:48:38 +0800

    update

--- emulator/dpi.cpp
+++ emulator/dpi.cpp
@@ -1,6 +1,7 @@
 #include "VSimTop__Dpi.h"
 #include "dpi.h"
 
+// C Env
 extern "C" void halt(int reason) {
 
 }
@@ -13,14 +14,9 @@ extern "C" void mem_write(int paddr, const svBitVecVal* mask, int data) {
 
 }
 
-extern "C" void read_diff_info(const svLogicVecVal *idx, svLogic *valid, svLogicVecVal *pc, svLogicVecVal *inst, svLogic *rf_wen, svLogicVecVal *rf_waddr, svLogicVecVal *rf_wdata, svLogic *mem_wen, svLogicVecVal *mem_addr, svLogicVecVal *mem_data, svLogicVecVal *mem_mask){
-    
-}
+// SV Env
+extern "C" void read_diff_info(const svLogicVecVal *idx, svLogic *valid, svLogicVecVal *pc, svLogicVecVal *inst, svLogic *rf_wen, svLogicVecVal *rf_waddr, svLogicVecVal *rf_wdata, svLogic *mem_wen, svLogicVecVal *mem_addr, svLogicVecVal *mem_data, svLogicVecVal *mem_mask);
 
-extern "C" void peek_arch_rat(const svLogicVecVal *a_reg, svLogicVecVal *p_reg) {
-    
-}
+extern "C" void peek_arch_rat(const svLogicVecVal *a_reg, svLogicVecVal *p_reg);
 
-extern "C" void peek_regfile(const svLogicVecVal *reg_idx, svLogicVecVal *reg_value) {
-    
-}
+extern "C" void peek_regfile(const svLogicVecVal *reg_idx, svLogicVecVal *reg_value);
